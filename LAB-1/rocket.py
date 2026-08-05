2e82fdc60e60f5b130e034c42a7fa6d2f0762656
Satya-Sai1729 <saitejaswi1729@gmail.com>
2026-08-05 23:55:38 +0530

    Refactor launch function for countdown using recursion
    Refactor countdown launch function to use recursion instead of a loop.

--- LAB-1/rocket.py
+++ LAB-1/rocket.py
@@ -1,4 +1,15 @@
 def launch(n):
+    if n==0:
+        print("Launch")
+    return
+    print(n)
+    launch(n-1)
+n=int(input("Enter the duration of the Countdown : "))
+launch(n)
+
+#Alternate
+"""
+def launch(n):
     while n>0:
         return n
     else:
@@ -7,3 +18,4 @@ n=int(input("Enter Number : "))
 while n>=0:
     print(launch(n))
     n-=1
+"""
